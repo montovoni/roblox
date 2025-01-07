b0b838478d4b0a7df5e31aebafe073e0b928877c
montovoni <57241037+montovoni@users.noreply.github.com>
2025-01-07 05:22:41 -0300

    Wicked RP
    Wicked RP

--- Scripts/18973713348.lua
+++ Scripts/18973713348.lua
@@ -10,14 +10,14 @@ end
 
 -- 2. Teleportar o Player para Cada Esmeralda
 
-local player = game.Players.LocalPlayer
-local character = player.Character or player.CharacterAdded:Wait()
-local remote = game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("InventoryService:CollectEmeraldEvent")
-local emeralds = workspace.Secured.EmeraldPickups:GetChildren()
+-- local player = game.Players.LocalPlayer
+-- local character = player.Character or player.CharacterAdded:Wait()
+-- local remote = game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("InventoryService:CollectEmeraldEvent")
+-- local emeralds = workspace.Secured.EmeraldPickups:GetChildren()
 
-for _, emerald in ipairs(emeralds) do
-    local position = emerald.Position
-    character:SetPrimaryPartCFrame(CFrame.new(position)) -- Move o personagem
-    task.wait(0.5) -- Espera para garantir que a posição seja atualizada no servidor
-    remote:FireServer(emerald:GetAttribute("ID") or _)
-end
+-- for _, emerald in ipairs(emeralds) do
+--     local position = emerald.Position
+--     character:SetPrimaryPartCFrame(CFrame.new(position)) -- Move o personagem
+--     task.wait(0.5) -- Espera para garantir que a posição seja atualizada no servidor
+--     remote:FireServer(emerald:GetAttribute("ID") or _)
+-- end
